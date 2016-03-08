4c3f409f3a129d62feee2c2d7d60c67e89d0adba
Nico Kratky <nico@nicokratky.me>
2016-03-08 21:09:52 +0100

    wrong usage of '\n'.. oops

--- setup.sh
+++ setup.sh
@@ -8,42 +8,42 @@ if [[ "`uname`" == "Darwin" ]]; then
 
     # Installing command line tools
     xcode-select --install
-    read -p "\nPress Enter when either the command line tools or Xcode are installed"
+    read -p "Press Enter when either the command line tools or Xcode are installed"
     # Making shure that the command line tools or Xcode are installed
     command -v clang >/dev/null 2>&1 || { echo "Command line tools aren't installed"; exit 1; }
 
     # Installing brew if not
     command -v brew >/dev/null 2>&1 || { ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
 
-    printf $BLUE "\nInstalling brew tools and cask apps"
+    printf "\n$BLUE" "Installing brew tools and cask apps"
 
     brew bundle
 
     # Making bash 4 default shell
-    printf $BLUE "\nChanging shell to bash for user $USER"
+    printf "\n$BLUE" "Changing shell to bash for user $USER"
 
     grep -q '/usr/local/bin/bash' /etc/shells || echo '/usr/local/bin/bash' | sudo tee -a /etc/shells
     chsh -s /usr/local/bin/bash
 
     # Installing RVM, ruby 2.2 and gems
     if [ ! -f $HOME/.rvm/scripts/rvm ]; then
-      printf $BLUE "\nInstalling RVM"
+      printf "\n$BLUE" "Installing RVM"
       gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
       curl -L get.rvm.io | bash -s stable
     else
-      printf $BLUE "\nRVM already installed"
+      printf "\n$BLUE" "RVM already installed"
     fi
 
     # Sourcing script to use rvm
     source $HOME/.rvm/scripts/rvm
 
     # Installing Ruby 2.2
-    printf $BLUE "\nInstalling Ruby 2.2"
+    printf "\n$BLUE" "Installing Ruby 2.2"
 
     rvm install 2.2
 
     # Installing Ruby gems
-    printf $BLUE "\nInstalling Ruby gems"
+    printf "\n$BLUE" "Installing Ruby gems"
 
     gems=("sass" "iStats")
     for gem in "${gems[@]}"; do
@@ -55,7 +55,7 @@ if [[ "`uname`" == "Darwin" ]]; then
     done
 
     # App Store Applications installation
-    printf $BLUE "\nInstalling Mac App Store applications"
+    printf "\n$BLUE" "Installing Mac App Store applications"
 
     apps=("497799835" "409183694" "890031187" "409201541" "557168941" "937984704" "409203825")
 
@@ -68,7 +68,7 @@ if [[ "`uname`" == "Darwin" ]]; then
     done
 
     # installing node modules
-    printf $BLUE "\nInstalling global node modules"
+    printf "\n$BLUE" "Installing global node modules"
 
     modules=("bower" "babel-cli" "nodemon" "yo" "gulp" "jade" "express-generator")
 
@@ -80,7 +80,7 @@ if [[ "`uname`" == "Darwin" ]]; then
       fi
     done
 
-    printf $BLUE "\nCopying dotfiles"
+    printf "\n$BLUE" "Copying dotfiles"
 
     # Copy dotfiles to home directory
     read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1 -r;
@@ -97,4 +97,4 @@ else
     fi
 fi
 
-printf $BLUE "Please run 'source ~/.bash_profile'"
+printf "\n$BLUE" "Please run 'source ~/.bash_profile'"
