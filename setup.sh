12b61d088947e72110879f0de6e0a8f7bfd2b3a7
Nico Kratky <nico@nicokratky.me>
2017-02-28 11:17:40 +0100

    excluded .editorconfig on linux systems

--- setup.sh
+++ setup.sh
@@ -87,7 +87,7 @@ else
     read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1 -r;
     echo ""
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        rsync --exclude ".git/" --exclude "apps.md" --exclude ".DS_Store" --exclude "setup.sh" --exclude "Brewfile" --exclude "README.md" --exclude ".hushlogin" --exclude ".osx" --exclude "iterm2/"-avh --no-perms . ~;
+        rsync --exclude ".git/" --exclude "apps.md" --exclude ".DS_Store" --exclude "setup.sh" --exclude "Brewfile" --exclude "README.md" --exclude ".hushlogin" --exclude ".osx" --exclude "iterm2/" --exclude ".editorconfig" -avh --no-perms . ~;
     fi
 fi
 
