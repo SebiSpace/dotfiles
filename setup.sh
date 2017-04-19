8002d9de9c62391ee06fd8ba32869e608c7c7819
Nico Kratky <nico@nicokratky.me>
2017-04-19 16:31:05 +0200

    added yeoman generators

--- setup.sh
+++ setup.sh
@@ -62,7 +62,7 @@ if [[ "`uname`" == "Darwin" ]]; then
     # installing node modules
     printf "\n$BLUE" "Installing global node modules"
 
-    modules=("bower" "babel-cli" "nodemon" "yo" "gulp" "pug" "express-generator" "fkill-cli")
+    modules=("bower" "babel-cli" "nodemon" "yo" "gulp-cli" "pug" "generator-express" "fkill-cli" "generator-webapp")
 
     installed="`npm list -g`"
 
